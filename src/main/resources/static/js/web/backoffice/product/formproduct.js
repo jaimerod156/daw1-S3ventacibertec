29cf1643c1f959caeb363547001fe117e0dfdc80
jaimerod156 <jaimerodriguez156@gmail.com>
2025-07-12 20:31:57 -0400

    product funciones javascript v2

--- src/main/resources/static/js/web/backoffice/product/formproduct.js
+++ src/main/resources/static/js/web/backoffice/product/formproduct.js
@@ -1,4 +1,15 @@
 $(document).on("click","#btnagregar", function(){
-  $("#modalproduct").modal("show");
+    $("#txtnomproduct").val("");
+    $("#txtunitprice").val("");
+    $("#hddproducto").val("0");
 
+  $("#modalproduct").modal("show");
+})
+
+$(document).on("click",".btnactualizar", function(){
+    $("#txtnomproduct").val($(this).attr("data-prodname"));
+    $("#txtunitprice").val($(this).attr("data-produnit"));
+    $("#hddproducto").val($(this).attr("data-prodcod"));
+
+    $("#modalproduct").modal("show");
 })
